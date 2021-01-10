c7d62ef7e85d76f9598259e30000dd07f0304eab
Sérgius Corrêa <sergiuscorrea@gmail.com>
2021-01-09 22:12:24 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css';
 import Navbar from './Navbar/Index';
+import Home from './Home/index';
 
 
 
 function App() {
   return (
-    <div> 
+    <> 
       <Navbar />
-    </div>
+      <Home />
+    </>
   );
 }
 
